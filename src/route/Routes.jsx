930eb7f4d8a6ac2e4e1b5b76df0e41696e784708
9hb_y <gyuhakarmy@gmail.com>
2025-02-25 10:34:04 +0900

    02.25

--- src/route/Routes.jsx
+++ src/route/Routes.jsx
@@ -1,6 +1,8 @@
 import BoardPage from "../pages/BoardPage"
+import ConditionalPage from "../pages/ConditionalPage"
 import HelloPage from "../pages/HelloPage"
 import HomePage from "../pages/HomePage"
+import JSXPage from "../pages/JSXPage"
 import ProfilePage from "../pages/ProfilePage"
 
 //React Routes, Route 사용시 배열로 관리하기
@@ -27,6 +29,18 @@ const routes = [
         path: '/hello',
         element: <HelloPage />,
         title: 'hello',
+    },
+
+    {
+        path: '/jsx',
+        element: <JSXPage />,
+        title: 'jsx문법',
+    },
+
+    {
+        path: '/conditional',
+        element: <ConditionalPage />,
+        title: '조건부랜더링',
     },
   ]
   export default routes
