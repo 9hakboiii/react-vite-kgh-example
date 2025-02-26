de4375cc6979ae8d9b7bb6bf6385b967ab9b1450
9hb_y <gyuhakarmy@gmail.com>
2025-02-26 11:36:26 +0900

    브라우저라우터 학습

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom'
 import routes from './route/Routes'
 import Header from './components/Header'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+
+// outlet을 사용하여, Router.jsx 파일의 path 결과에 따라 
+// root(App.jsx)를 기준으로 element를 넘김.
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          {routes.map((route, index) => {
-            return <Route key={route.path || index} path={route.path} element={route.element}></Route>
-          })}
-        </Routes>
-      </BrowserRouter>
+      <div>헤더 입니다</div>
+      <Outlet /> 
+      <div>꼬리말</div>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import App from './App.jsx'
 import App2 from './App2.jsx'
+import { RouterProvider } from 'react-router-dom'
+import router from './route/Router.jsx'
 
 createRoot(document.getElementById('root')).render(
-  // <StrictMode>
-    // <App />
-    <App2 />
-  // </StrictMode>,
+  <RouterProvider router={router}/>
 )
+
+
+// 기존 방식
+// createRoot(document.getElementById('root')).render(
+//   // <StrictMode>
+//     // <App />
+//     <App2 />
+//   // </StrictMode>,
+// )
